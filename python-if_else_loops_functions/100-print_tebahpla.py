a1adf35bbbb1b16f0e7c37f311cf61d6ecf05098
bassem-krayem <bassem.krayem09@gmail.com>
2024-05-09 22:39:39 +0100

    task13: #advance: Smile in the mirror

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,6 +2,6 @@
 for x in range(ord('z'), ord('a') - 1, - 1):
     lettre = chr(x)
     if x % 2 != 0:
-        lettre  = chr(x - 32)
+        lettre = chr(x - 32)
 
     print("{}".format(lettre), end="")
