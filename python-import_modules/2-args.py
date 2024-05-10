2e42a1cd449904495160b08380b427d0e9f4249f
bassem-krayem <bassem.krayem09@gmail.com>
2024-05-10 08:30:44 +0100

    task2: How to make a script dynamic!

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
 import sys
 
-
 def arguments():
     args = sys.argv
     num_args = len(args) - 1
-    if num_args == 1:
+    if num_args == 0:
         print("0 arguments")
     else:
-        print(f"{num_args} arguments")
+        print(f"{num_args} argument")
 
     for x in range(1, len(args)):
         print(f"{x}: {args[x]}")
 
 if __name__ == "__main__":
     arguments()
-
-arguments()
