3bc0f807597c833003d735651dc836f4f89827a5
bassem-krayem <bassem.krayem09@gmail.com>
2024-05-17 19:25:12 +0100

    task12: Roman to Integer

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string and type(roman_string) == str:
-        convert_dict = {'I': 1,
-                        'V': 5,
-                        'X': 10,
-                        'L': 50,
-                        'C': 100,
-                        'D': 500,
-                        'M': 1000}
+    if roman_string and isinstance(roman_string, str):
+        convert_dict = {
+            'I': 1, 'V': 5, 'X': 10, 'L': 50,
+            'C': 100, 'D': 500, 'M': 1000
+        }
         convert_list = []
 
         for i in range(len(roman_string)):
-            current_value = convert_dict[roman_string[i]]
+            current_value = convert_dict.get(roman_string[i], 0)
 
-            if i > 0 and convert_dict[roman_string[i-1]] < current_value:
+            if (i > 0 and convert_dict.get(roman_string[i - 1], 0)
+                    < current_value):
                 convert_list[-1] *= -1
             convert_list.append(current_value)
 
