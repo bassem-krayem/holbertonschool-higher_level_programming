398daf4f1472184e9d26f976888568ad35897002
bassem-krayem <bassem.krayem09@gmail.com>
2024-05-17 19:04:54 +0100

    task10: Best score

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-def best_score(a_dictionary):
-    if a_dictionary:
-        max_value = max(a_dictionary, key=a_dictionary.get)
-        return max_value
-    else:
-        return None
-    
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+    for row in matrix:
+        new_row = []
+        for x in row:
+            new_row.append(x ** 2)
+        new_matrix.append(new_row)  # Correctly indented
+    return new_matrix
