7f89a8b144c04afe0b8ca3c09c4f9945d83924b2
bassem-krayem <bassem.krayem09@gmail.com>
2024-05-20 22:27:25 +0100

    task1: Square with size

---
+++ python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# 1-square.py
+"""a class Square that defines a square by: (based on 0-square.py)."""
+
+
+class Square:
+    """ a class names square."""
+    
+    def __init__(self, size):
+        """ the size atribute."""
+        self.__size = size
