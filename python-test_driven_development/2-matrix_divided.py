4a432465755469f5a1a8bedef088b65daced66f1
bassem-krayem <bassem.krayem09@gmail.com>
2024-05-22 18:00:41 +0100

    task1: Divide a matrix

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -34,8 +34,7 @@ def matrix_divided(matrix, div):
 
     return ([list(map(lambda x: round(x / div, 2), row)) for row in matrix])
 
+
 if __name__ == "__main__":
     import doctest
-
     doctest.testfile("tests/2-matrix_divided.txt")
-    
