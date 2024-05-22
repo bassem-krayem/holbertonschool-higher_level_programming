33ea8ec7d8fd0f274129f8bb73b1dd38c44d77c6
bassem-krayem <bassem.krayem09@gmail.com>
2024-05-22 18:25:34 +0100

    task4: text indentation

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Module prints a text with 2 new lines after each of these characters: ., ?, and :.
+Module prints a text with 2 new lines after each of these characters:
+., ?, and :.
 """
 
 
