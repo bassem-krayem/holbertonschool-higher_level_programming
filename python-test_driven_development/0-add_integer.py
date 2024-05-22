20327a75a8519c0eee1bbf56712c94d01403819f
bassem-krayem <bassem.krayem09@gmail.com>
2024-05-22 15:57:56 +0100

    task0: add two integers

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -19,7 +19,7 @@ def add_integer(a, b=98):
     if not isinstance(b, (int, float)) or isinstance(b, bool):
         raise TypeError("b must be an integer")
 
-    return int(a) + int(b)
+    return int(a + b)
 
 
 if __name__ == "__main__":
