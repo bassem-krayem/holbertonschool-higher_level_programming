322c888fb777d882790a5e4c55492d610ce606a0
bassem-krayem <bassem.krayem09@gmail.com>
2024-05-28 14:48:46 +0100

    task0: Read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -2,17 +2,20 @@
 """
 This script defines a function to read and print the contents of a file.
 
-The function `read_file` takes a filename as an argument, reads the content of the file,
+The function `read_file` takes a filename as an argument,
+reads the content of the file,
 and prints it to the standard output.
 """
+
 
 def read_file(filename=""):
     """
     Reads the contents of a file and prints it to the standard output.
-    
+
     Args:
-        filename (str): The name of the file to be read. Defaults to an empty string.
-    
+        filename (str): The name of the file to be read.
+        Defaults to an empty string.
+
     Raises:
         FileNotFoundError: If the file does not exist.
         IOError: If an I/O error occurs while reading the file.
@@ -21,6 +24,6 @@ def read_file(filename=""):
     with open(filename, "r") as file:
         # Read the entire content of the file
         file_content = file.read()
-    
+
     # Print the file content, end="" ensures no additional newline is added
     print(file_content, end="")
