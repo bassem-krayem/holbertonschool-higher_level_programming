c736ccd346430d53ae9b6ad0d3faac66629b3b6d
bassem-krayem <bassem.krayem09@gmail.com>
2024-06-01 19:45:41 +0100

    task 03:Develop a simple API using Python with the  module

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -2,6 +2,7 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import json
 
+
 class SimpleHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         if self.path == '/':
@@ -29,6 +30,7 @@ def run(server_class=HTTPServer, handler_class=SimpleHandler, port=8000):
     httpd = server_class(server_address, handler_class)
     print(f"Starting httpd server on port {port}")
     httpd.serve_forever()
+
 
 if __name__ == "__main__":
     run()
