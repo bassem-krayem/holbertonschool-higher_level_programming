4c2089d48014954bdf6978bd9546e95cfa5ccf5f
bassem-krayem <bassem.krayem09@gmail.com>
2024-06-01 21:05:03 +0100

    task 04: Develop a Simple API using Python with Flask

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -1,13 +1,10 @@
-#!/usr/bin/python3
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 users = {
-    "jane": {"username": "jane", "name": "Jane", "age": 28, "city":
-             "Los Angeles"},
-    "john": {"username": "john", "name": "John", "age": 30, "city":
-             "New York"}
+    "jane": {"username": "jane", "name": "Jane", "age": 28, "city": "Los Angeles"},
+    "john": {"username": "john", "name": "John", "age": 30, "city": "New York"}
 }
 
 
@@ -49,3 +46,4 @@ def add_user():
 
 if __name__ == "__main__":
     app.run(debug=True)
+    
