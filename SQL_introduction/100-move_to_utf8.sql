66824ad2795ee4c670aac5424ffad364cb04bad1
bassem-krayem <bassem.krayem09@gmail.com>
2024-06-20 21:35:28 +0100

    task17 advanced: convert to utf8mb4

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -9,7 +9,7 @@ ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode
 
 -- Explicitly change the 'name' column to UTF-8mb4, adjusting the size to VARCHAR(191) 
 -- (recommended for compatibility with MySQL's indexing limitations)
-ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table MODIFY name VARCHAR(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- Additionally, convert the 'id' column and set the default collation for the table
 ALTER TABLE first_table MODIFY id INT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
