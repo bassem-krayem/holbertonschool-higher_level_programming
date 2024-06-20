1c862c74989174def7e6df010274fc07b7ca136b
bassem-krayem <bassem.krayem09@gmail.com>
2024-06-20 21:11:41 +0100

    task16: Say my name

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,4 +1,5 @@
-
+-- This script lists all records of the table second_table from the database hbtn_0c_0.
+-- It only displays records that have a name value, sorted by score in descending order.
 
 SELECT score, name
 FROM second_table
