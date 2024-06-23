9de2d319e73c544076b67bda6ede641c85804cc5
bassem-krayem <bassem.krayem09@gmail.com>
2024-06-23 20:54:05 +0100

    task8: selecting cities of Ca by using sub quiries

---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,19 @@
+-- Retrieve Cities in California
+--
+-- This SQL query selects the names of all cities located in the state of California from 
+-- the 'hbtn_0d_usa' database. It does this by:
+--
+-- 1. Subquery: 
+--    - The inner query finds the unique ID of the state named "California" in the 'states' table.
+--
+-- 2. Main Query:
+--    - The outer query selects the 'name' column from the 'cities' table.
+--    - It filters the results to include only cities where the 'state_id' matches the ID of California obtained from the subquery.
+
+SELECT id, name 
+FROM hbtn_0d_usa.cities 
+WHERE state_id = (
+    SELECT id 
+    FROM hbtn_0d_usa.states 
+    WHERE name = "California"
+);
