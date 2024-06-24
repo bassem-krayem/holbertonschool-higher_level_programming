c4c3307bb330f5e985a143992434dbe217cb1a3d
bassem-krayem <bassem.krayem09@gmail.com>
2024-06-24 08:09:14 +0100

    task16: -- Retrieve All TV Shows and Their Genres (Including Shows Without Genres)

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,20 +1,10 @@
--- Retrieve All TV Shows and Their Genres (Including Shows Without Genres)
+-- lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows.
 
--- This query retrieves a list of all TV shows in the 'tv_shows' table, along with their 
--- associated genres from the 'tv_genres' table.
+SELECT tv_shows.title, tv_genres.name
+  FROM tv_shows
+       LEFT JOIN tv_show_genres
+       ON tv_shows.id = tv_show_genres.show_id
 
-SELECT 
-    tv_shows.title,          -- Select the show title from the 'tv_shows' table
-    tv_genres.name AS genre  -- Select the genre name (or NULL if not linked)
-
-FROM tv_shows                 -- Start with the 'tv_shows' table to get ALL shows
-
--- Perform LEFT JOINs to include shows even if they don't have genres:
-LEFT JOIN tv_show_genres      
-ON tv_shows.id = tv_show_genres.show_id  -- Link shows to their genres (if they exist)
-
-LEFT JOIN tv_genres
-ON tv_show_genres.genre_id = tv_genres.id  -- Link show-genre pairs to the genre names
-
--- Order the results by show title first, then by genre name (NULLs will appear last):
-ORDER BY tv_shows.title ASC, tv_genres.name ASC;
+       LEFT JOIN tv_genres
+       ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY tv_shows.title, tv_genres.name ASC;
