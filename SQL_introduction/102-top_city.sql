42695fafe5a7b9c33d8206797eb6bb7d32adb780
bassem-krayem <bassem.krayem09@gmail.com>
2024-09-30 10:46:35 +0100

    task19

--- SQL_introduction/102-top_city.sql
+++ SQL_introduction/102-top_city.sql
@@ -1,3 +1,9 @@
 -- a script to display
 
-select city, avg(value) as avg_temp from temperatures where month = 7 or month = 8 group by city order by avg_temp DESC limit 3;
+SELECT city, 
+       AVG(value) AS avg_temp 
+FROM temperatures 
+WHERE month = 7 OR month = 8 
+GROUP BY city 
+ORDER BY avg_temp DESC 
+LIMIT 3;
